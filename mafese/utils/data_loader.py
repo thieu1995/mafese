32638d1e79cca544fb0a6dbefb4591aa4a90708e
Thieu Nguyen <nguyenthieu2102@gmail.com>
2023-05-25 11:22:30 +0700

    Update data_loader module

--- mafese/utils/data_loader.py
+++ mafese/utils/data_loader.py
@@ -7,15 +7,22 @@
 import pandas as pd
 import numpy as np
 from pathlib import Path
+from sklearn.model_selection import train_test_split
 
 
 class Data:
     #  structure for the data (not dataset)
-    def __init__(self, features, labels):
-        self.features = features
-        self.labels = labels
+    def __init__(self, X, y):
+        self.X = X
+        self.y = y
         self.X_train, self.y_train, self.X_test, self.y_test = None, None, None, None
-        self.classifier = None
+
+    def split_train_test(self, test_size=0.2, train_size=None,
+                         random_state=41, shuffle=True, stratify=None, inplace=True):
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.X, self.y, test_size=test_size,
+                        train_size=train_size, random_state=random_state, shuffle=shuffle, stratify=stratify)
+        if not inplace:
+            return self.X_train, self.X_test, self.y_train, self.y_test
 
 
 def get_dataset(dataset_name):
@@ -25,7 +32,7 @@ def get_dataset(dataset_name):
     list_datasets = [pathfile.name[:-4] for pathfile in list_paths]
 
     if dataset_name not in list_datasets:
-        print(f"MHA-FS currently does not have {dataset_name} in its database....")
+        print(f"MAFESE currently does not have {dataset_name} in its database....")
         display = input("Enter 1 to see the available datasets: ") or 0
         if display:
             for idx, dataset in enumerate(list_datasets):
@@ -33,12 +40,7 @@ def get_dataset(dataset_name):
         exit(0)
     else:
         df = pd.read_csv(f"{dir_root}/{dataset_name}.csv", header=None)
-        data = Data(features=np.array(df.iloc[:, 0:-1]), labels=np.array(df.iloc[:, -1]))
+        data = Data(np.array(df.iloc[:, 0:-1]), np.array(df.iloc[:, -1]))
         print(f"Requested dataset: {dataset_name} found and loaded...")
         # print(df.head(10))
         return data
-
-#
-# if __name__ == '__main__':
-#     data = get_dataset('WaveformEW1')
-
