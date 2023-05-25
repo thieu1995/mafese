585476ef15e2b1932d4a25517443b455769e07f4
Thieu Nguyen <nguyenthieu2102@gmail.com>
2023-05-25 12:16:53 +0700

    Update init file

--- mafese/__init__.py
+++ mafese/__init__.py
@@ -4,4 +4,9 @@
 #       Github: https://github.com/thieu1995        %                         
 # --------------------------------------------------%
 
+__version__ = "0.1.0"
+
 from mafese.utils.data_loader import Data, get_dataset
+from mafese.filter import Filter
+from mafese.wrapper.recursive import Recursive
+from mafese.wrapper.sequential import Sequential
