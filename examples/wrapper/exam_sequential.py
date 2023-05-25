4e87a28a7bfed1ff2fe464ab88b80ad758297314
Thieu Nguyen <nguyenthieu2102@gmail.com>
2023-05-25 11:29:16 +0700

    Add example for Recursive and Sequential class

---
+++ examples/wrapper/exam_sequential.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# Created by "Thieu" at 16:41, 24/05/2023 ----------%                                                                               
+#       Email: nguyenthieu2102@gmail.com            %                                                    
+#       Github: https://github.com/thieu1995        %                         
+# --------------------------------------------------%
+
+
+def get_dataset(data_type="classification"):
+    if data_type == "classification":
+        from sklearn.datasets import load_iris
+
+        X, y = load_iris(return_X_y=True)
+        print(X.shape)
+        print(f"X: {X[:1]}, y: {y[:1]}")
+        return X, y
+    else:
+        from sklearn.datasets import load_diabetes
+
+        X, y = load_diabetes(return_X_y=True)               # Load the diabetes dataset
+        print(X.shape)
+        print(f"X: {X[:1]}, y: {y[:1]}")
+        return X, y
+
+
+def get_sequential_from_scikit_learn(data_type, X, y):
+    from sklearn.feature_selection import SequentialFeatureSelector
+    from sklearn.neighbors import KNeighborsClassifier, KNeighborsRegressor
+
+    ## Using scikit-learn library
+    print("=============Using scikit-learn library===============")
+    if data_type == "regression":
+        estimator = KNeighborsRegressor(n_neighbors=5)
+    else:
+        estimator = KNeighborsClassifier(n_neighbors=5)
+    feat_selector = SequentialFeatureSelector(estimator=estimator, n_features_to_select=3, direction="forward")
+    feat_selector.fit(X, y)
+    X_selected = feat_selector.transform(X)
+    print(X_selected.shape)
+    print(X_selected[:1])
+
+
+def get_sequential_from_mafese(data_type, X, y):
+    from mafese.wrapper.sequential import Sequential
+
+    ## Using Mafese library
+    print("=============Using Mafese library===============")
+    feat_selector = Sequential(problem=data_type, estimator="knn", n_features=3, direction="forward")
+    feat_selector.fit(X, y)
+    X_selected = feat_selector.transform(X)
+    print(X_selected.shape)
+    print(X_selected[:1])
+    print(feat_selector.selected_feature_masks)
+    print(feat_selector.selected_feature_solution)
+    print(feat_selector.selected_feature_indexes)
+
+
+data_type = "regression"        # classification
+X, y = get_dataset(data_type)
+get_sequential_from_scikit_learn(data_type, X, y)
+get_sequential_from_mafese(data_type, X, y)
