195e67c3038e2cbcd382877c0813fdfc3d0e9f35
Thieu Nguyen <nguyenthieu2102@gmail.com>
2023-05-25 14:28:27 +0700

    Add tests

---
+++ tests/test_filter.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# Created by "Thieu" at 14:16, 25/05/2023 ----------%                                                                               
+#       Email: nguyenthieu2102@gmail.com            %                                                    
+#       Github: https://github.com/thieu1995        %                         
+# --------------------------------------------------%
+
+import numpy as np
+from mafese.filter import Filter
+
+np.random.seed(42)
+
+
+def test_Filter_class():
+    X = np.random.rand(10, 6)
+    y = np.random.randint(0, 2, size=10)
+    n_features = 3
+    feat_selector = Filter(problem="classification", method="ANOVA", n_features=3)
+    feat_selector.fit(X, y)
+    X_selected = feat_selector.transform(X)
+    assert X_selected.shape[1] == n_features
+    assert len(feat_selector.selected_feature_indexes) == n_features
---
+++ tests/test_wrapper.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# Created by "Thieu" at 14:15, 25/05/2023 ----------%                                                                               
+#       Email: nguyenthieu2102@gmail.com            %                                                    
+#       Github: https://github.com/thieu1995        %                         
+# --------------------------------------------------%
+
+import numpy as np
+from mafese.wrapper.sequential import Sequential
+from mafese.wrapper.recursive import Recursive
+
+np.random.seed(42)
+
+
+def test_Sequential_class():
+    X = np.random.rand(10, 6)
+    y = np.random.randint(0, 2, size=10)
+    n_features = 3
+    feat_selector = Sequential(problem="regression", estimator="knn", n_features=n_features, direction="forward")
+    feat_selector.fit(X, y)
+    X_selected = feat_selector.transform(X)
+    assert X_selected.shape[1] == n_features
+    assert len(feat_selector.selected_feature_indexes) == n_features
+
+
+def test_Recursive_class():
+    X = np.random.rand(10, 6)
+    y = np.random.randint(0, 2, size=10)
+    n_features = 3
+    feat_selector = Recursive(problem="classification", estimator="rf", n_features=3)
+    feat_selector.fit(X, y)
+    X_selected = feat_selector.transform(X)
+    assert X_selected.shape[1] == n_features
+    assert len(feat_selector.selected_feature_indexes) == n_features
