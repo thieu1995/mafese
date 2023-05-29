9fc51900b2d5e843cc0a803b3b214d702c4a2b35
Thieu Nguyen <nguyenthieu2102@gmail.com>
2023-05-29 16:34:18 +0700

    Update __init__

--- mafese/__init__.py
+++ mafese/__init__.py
@@ -4,7 +4,7 @@
 #       Github: https://github.com/thieu1995        %                         
 # --------------------------------------------------%
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 from mafese.utils.data_loader import Data, get_dataset
 from mafese.filter import FilterSelector
