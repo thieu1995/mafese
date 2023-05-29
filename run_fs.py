c9e806958178364b987c084aa6aa8bf530d66e50
Thieu Nguyen <nguyenthieu2102@gmail.com>
2023-05-29 15:41:34 +0700

    Update example for MhaSelector

--- run_fs.py
+++ run_fs.py
@@ -6,20 +6,28 @@
 
 from mafese.wrapper.mha import MhaSelector
 from mafese import get_dataset
+from mafese import evaluator
+from sklearn.svm import SVC
 
 
 data = get_dataset("Arrhythmia")
 data.split_train_test(test_size=0.2)
+print(data.X_train.shape, data.X_test.shape)            # (361, 279) (91, 279)
+
 
 feat_selector = MhaSelector(problem="classification", estimator="knn",
                             optimizer="BaseGA", optimizer_paras=None,
                             transfer_func="vstf_01", obj_name="AS")
 feat_selector.fit(data.X_train, data.y_train, fit_weights=(0.9, 0.1), verbose=True)
-X_selected = feat_selector.transform(data.X_test)
-print(feat_selector.get_best_obj_and_fit())             # {'obj': 0.94139, 'fit': 0.8474085293158468}
+X_selected = feat_selector.transform(data.X_train)
+print(feat_selector.get_best_obj_and_fit())             # {'obj': 0.83504, 'fit': 0.7424429876435843}
 print(f"Original Dataset: {data.X_train.shape}")        # Original Dataset: (361, 279)
-print(f"Selected dataset: {X_selected.shape}")          # Selected dataset: (91, 20)
+print(f"Selected dataset: {X_selected.shape}")          # Selected dataset: (361, 31)
 
 print(feat_selector.selected_feature_masks)
 print(feat_selector.selected_feature_solution)
 print(feat_selector.selected_feature_indexes)
+
+results = evaluator.evaluate(feat_selector, estimator=SVC(), data=data, metrics=["AS", "PS", "RS"])
+print(results)
+# {'AS_train': 0.77176, 'PS_train': 0.54177, 'RS_train': 0.6205, 'AS_test': 0.72636, 'PS_test': 0.34628, 'RS_test': 0.52747}
