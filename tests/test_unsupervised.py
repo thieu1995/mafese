903926086f98d7a8e2094d954739e53d0b6351c4
Thieu Nguyen <nguyenthieu2102@gmail.com>
2023-06-01 15:52:10 +0700

    Update tests

--- tests/test_unsupervised.py
+++ tests/test_unsupervised.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python
-# Created by "Thieu" at 14:16, 25/05/2023 ----------%                                                                               
+# Created by "Thieu" at 09:54, 01/06/2023 ----------%                                                                               
 #       Email: nguyenthieu2102@gmail.com            %                                                    
 #       Github: https://github.com/thieu1995        %                         
 # --------------------------------------------------%
 
 import numpy as np
-from mafese.filter import FilterSelector
+from mafese.unsupervised import UnsupervisedSelector
 
 np.random.seed(42)
 
 
-def test_FilterSelector_class():
+def test_UnsupervisedSelector_class():
     X = np.random.rand(10, 6)
     y = np.random.randint(0, 2, size=10)
     n_features = 3
-    feat_selector = FilterSelector(problem="classification", method="ANOVA", n_features=3)
+    feat_selector = UnsupervisedSelector(problem="classification", method="MCL", n_features=3)
     feat_selector.fit(X, y)
     X_selected = feat_selector.transform(X)
     assert X_selected.shape[1] == n_features
