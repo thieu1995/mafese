31f9d900b6910516871ca846dca5758f879dd19d
Thieu Nguyen <nguyenthieu2102@gmail.com>
2025-05-30 23:13:11 +0700

    Add ObjectiveScaler class to encoder module

--- mafese/utils/encoder.py
+++ mafese/utils/encoder.py
@@ -79,3 +79,39 @@ class LabelEncoder:
         if self.unique_labels is None:
             raise ValueError("Label encoder has not been fit yet.")
         return np.array([self.unique_labels[i] if i in self.label_to_index.values() else "unknown" for i in y])
+
+
+class ObjectiveScaler:
+    """
+    For label scaler in classification (binary and multiple classification)
+    """
+    def __init__(self, obj_name="sigmoid", ohe_scaler=None):
+        """
+        ohe_scaler: Need to be an instance of One-Hot-Encoder for softmax scaler (multiple classification problem)
+        """
+        self.obj_name = obj_name
+        self.ohe_scaler = ohe_scaler
+
+    def transform(self, data):
+        if self.obj_name == "sigmoid" or self.obj_name == "self":
+            return data
+        elif self.obj_name == "hinge":
+            data = np.squeeze(np.array(data))
+            data[np.where(data == 0)] = -1
+            return data
+        elif self.obj_name == "softmax":
+            data = self.ohe_scaler.fit_transform(np.reshape(data, (-1, 1)))
+            return data
+
+    def inverse_transform(self, data):
+        if self.obj_name == "sigmoid":
+            data = np.squeeze(np.array(data))
+            data = np.rint(data).astype(int)
+        elif self.obj_name == "hinge":
+            data = np.squeeze(np.array(data))
+            data = np.ceil(data).astype(int)
+            data[np.where(data == -1)] = 0
+        elif self.obj_name == "softmax":
+            data = np.squeeze(np.array(data))
+            data = np.argmax(data, axis=1)
+        return data
