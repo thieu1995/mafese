7eef933967a14c20cc400454ac5651e2fa0bae0c
anh9895 <honganh090895@gmail.com>
2025-06-02 13:54:25 +0700

    Update mealpy_util module

--- mafese/utils/mealpy_util.py
+++ mafese/utils/mealpy_util.py
@@ -5,15 +5,37 @@
 # --------------------------------------------------%
 
 import numpy as np
-from mealpy.optimizer import Optimizer
-from mealpy import *
+from mealpy import Problem
 
 
 class FeatureSelectionProblem(Problem):
-    def __init__(self, minmax=None, data=None, estimator=None, metric_class=None,
-                 obj_name=None, obj_paras=None, fit_weights=(0.9, 0.1), fit_sign=None,
-                 transfer_func="vstf_01", **kwargs):
-        bounds = TransferBinaryVar(n_vars=data.X.shape[1], tf_func=transfer_func, lb=-8, ub=8, all_zeros=False, name="my_var")
+    """
+    A class to define a feature selection optimization problem.
+
+    Attributes
+    ----------
+    data : object
+        An object containing training and testing datasets.
+    estimator : object
+        A machine learning model with `fit` and `predict` methods.
+    metric_class : object
+        A class used to evaluate the performance of the model.
+    obj_name : str
+        The name of the objective metric to optimize.
+    obj_paras : dict
+        Parameters for the objective metric.
+    fit_weights : tuple
+        Weights for combining the objective value and feature selection ratio.
+    fit_sign : int
+        Sign to determine the direction of optimization (e.g., 1 for maximization, -1 for minimization).
+
+    Methods
+    -------
+    obj_func(solution)
+        Computes the fitness, objective value, and number of selected features for a given solution.
+    """
+    def __init__(self, bounds=None, minmax=None, data=None, estimator=None, metric_class=None,
+                 obj_name=None, obj_paras=None, fit_weights=(0.9, 0.1), fit_sign=None, **kwargs):
         super().__init__(bounds, minmax, **kwargs)
         self.data = data
         self.estimator = estimator
@@ -25,11 +47,24 @@ class FeatureSelectionProblem(Problem):
         self.fit_sign = fit_sign
 
     def obj_func(self, solution):
-        x = self.decode_solution(solution)["my_var"]
+        """
+        Computes the fitness, objective value, and number of selected features for a given solution.
+
+        Parameters
+        ----------
+        solution : array-like
+            The solution representing selected features.
+
+        Returns
+        -------
+        list
+            A list containing the fitness value, objective value, and number of selected features.
+        """
+        x = self.decode_solution(solution)["my_var"]    # Please don't change this line, it is used to decode the solution
         cols = np.flatnonzero(x)
         self.estimator.fit(self.data.X_train[:, cols], self.data.y_train)
         y_valid_pred = self.estimator.predict(self.data.X_test[:, cols])
         evaluator = self.metric_class(self.data.y_test, y_valid_pred)
         obj = evaluator.get_metric_by_name(self.obj_name, paras=self.obj_paras)[self.obj_name]
         fitness = self.fit_weights[0]*obj + self.fit_weights[1]*self.fit_sign*(np.sum(x) / self.n_dims)
-        return [fitness, obj, np.sum(x)]
+        return [fitness, obj, np.sum(x)]    # Return fitness, objective value, and number of selected features
