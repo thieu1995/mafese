9c46e85323527eddc1626ffed16882b8ec267439
anh9895 <honganh090895@gmail.com>
2025-06-03 23:17:01 +0700

    Update run_fs file

--- run_fs.py
+++ run_fs.py
@@ -12,12 +12,13 @@ data = get_dataset("ecoli")
 data.split_train_test(test_size=0.2, random_state=2)
 print(data.X_train.shape, data.X_test.shape)            # (361, 279) (91, 279)
 
-feat_selector = MhaSelector(problem="classification", estimator="knn",
+feat_selector = MhaSelector(problem="classification", obj_name="AS",
+                            estimator="knn", estimator_paras=None,
                             optimizer="OriginalTLO", optimizer_paras={"epoch": 50, "pop_size": 30},
-                            transfer_func="vstf_01", obj_name="AS")
-feat_selector.fit(data.X_train, data.y_train, fit_weights=(0.9, 0.1), verbose=True)
+                            mode='single', n_workers=None, termination=None, seed=None, verbose=True)
+feat_selector.fit(data.X_train, data.y_train, fit_weights=(0.9, 0.1))
 X_selected = feat_selector.transform(data.X_train)
-print(feat_selector.get_best_obj_and_fit())             # {'obj': 0.83504, 'fit': 0.7424429876435843}
+print(feat_selector.get_best_information())             # {'obj': 0.83504, 'fit': 0.7424429876435843, 'n_columns': 2}
 print(f"Original Dataset: {data.X_train.shape}")        # Original Dataset: (361, 279)
 print(f"Selected dataset: {X_selected.shape}")          # Selected dataset: (361, 31)
 
@@ -27,4 +28,4 @@ print(feat_selector.selected_feature_indexes)
 
 results = feat_selector.evaluate(estimator=SVC(), data=data, metrics=["AS", "PS", "RS"])
 print(results)
-# {'AS_train': 0.77176, 'PS_train': 0.54177, 'RS_train': 0.6205, 'AS_test': 0.72636, 'PS_test': 0.34628, 'RS_test': 0.52747}
+# {'AS_train': 0.9310258409445311, 'PS_train': 0.7407825454641211, 'RS_train': 0.7985074626865671, 'AS_test': 0.9167387543252595, 'PS_test': 0.7893032212885155, 'RS_test': 0.8088235294117647}
